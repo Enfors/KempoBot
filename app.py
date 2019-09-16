49f19700afd6975b23ee4d225c0e89a34ea9c340
Christer Enfors <christer.enfors@gmail.com>
2019-09-16 21:19:45 +0200

    Line ending changes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from enforsml.text import nlp, utils
 
 app = Flask(__name__)
 
+
 class DemoBot():
 
     def __init__(self):
@@ -16,13 +17,13 @@ class DemoBot():
              "Vad är Shorinji Kempo för slags kampsport",
              "Shorinji Kempo är en mångsidig japansk kampsport, "
              "där vi bland annat använder sparkar, slag, kast "
-             "och losstagningar (när man blir fasthållen). " 
+             "och losstagningar (när man blir fasthållen). "
              '<iframe width="800" height="450" '
              'src="https://www.youtube.com/embed/fr1pk8r_-Vo" '
              'rameborder="0" allow="accelerometer; autoplay; '
              'encrypted-media; gyroscope; picture-in-picture" '
              'allowfullscreen></iframe>'],
-            
+
             # Hur blir man medlem
             ["Hur blir man medlem",
              "Hur blir man medlem i Shorinji Kempo",
@@ -37,13 +38,13 @@ class DemoBot():
              "Information om hur man blir medlem hittar du på "
              "<a href='http://shorinjikempo.net/traning/borja-trana'>"
              "sidan \"Börja träna\" på vår hemsida</a>."],
-            
+
             # Är det roligt
             ["Är det roligt att träna Shorinji Kempo",
              "Är Shorinji Kempo roligt",
              "Är det kul att träna",
              "Det är jätteroligt att träna Shorinji Kempo!"],
-            
+
             # Vem grundade Shorinji Kempo
             ["Vem grundade Shorinji Kempo",
              "Vem var det som grundade Shorinji Kempo",
@@ -53,7 +54,7 @@ class DemoBot():
              "Vem hittade på Shorinji Kempo",
              "Vem startade Shorinji Kempo",
              "Shorinji Kempo grundades av So Doshin."],
-            
+
             # Skillnad mot andra kampsporter
             ["Vad skiljer Shorinji Kempo från andra kampsporter",
              "Vad är det för skillnad mellan Shorinji Kempo och "
@@ -67,7 +68,7 @@ class DemoBot():
              "Shorinji Kempo brukar kallas \"Den tänkande människans "
              "kampsport\", eftersom vi lägger relativt stor vikt "
              "vid filosofi."],
-            
+
             # Finns det sparkar och slag
             ["Finns det sparkar och slag i Shorinji Kempo",
              "Finns det slag i Shorinji Kempo",
@@ -76,7 +77,7 @@ class DemoBot():
              "Sparkar man i Shorinji Kempo",
              "Shorinji Kempo innehåller sparkar, slag, "
              "losstagningar, nedtagningar och kast."],
-            
+
             # Gör det ont
             ["Gör det ont att träna Shorinji Kempo",
              "Får man ont av att träna",
@@ -86,7 +87,7 @@ class DemoBot():
              "ont, så det är viktigt att vi är försiktiga med "
              "varandra. Dessa tekniker tränas dock inte i "
              "juniorgruppen."],
-            
+
             # Vad kostar det
             ["Vad kostar det att vara medlem",
              "Vad kostar det att bli medlem i klubben",
@@ -100,7 +101,7 @@ class DemoBot():
              "Alla priser finns på "
              "<a href='http://shorinjikempo.net/traning/borja-trana'>"
              "vår hemsida</a>."],
-            
+
             # Provträna gratis
             ["Kan man provträna gratis",
              "Måste man bli medlem innan man tränar",
@@ -109,7 +110,7 @@ class DemoBot():
              "Måste man betala om man bara vill testa",
              "Om man betalar månadsavgiften, så kan man provträna en "
              "månad utan att behöva betala träningsavgift."],
-            
+
             # Var ligger dojon
             ["Var ligger träningslokalen",
              "Var finns dojon",
@@ -127,11 +128,18 @@ class DemoBot():
              "Vi tränar i Mariebergsskolans gamla gymnastiksal, "
              "på Rosenborgsgatan 28 i Karlstad. Det ligger snett "
              "över vägen från sjukhuset.<br>"
-             '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2032.6166318831747!2d13.478893316077826!3d59.37273698167405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465cb1795c9a4321%3A0x136c6cb621aa9bd!2sShorinji%20Kempo%20Karlstad%20Shibu!5e0!3m2!1ssv!2sse!4v1568641561509!5m2!1ssv!2sse" width="800" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>'
+             '<iframe src="https://www.google.com/maps/embed?pb='
+             '!1m18!1m12!1m3!1d2032.6166318831747!2d13.478893316077826!'
+             '3d59.37273698167405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!'
+             '4f13.1!3m3!1m2!1s0x465cb1795c9a4321%3A0x136c6cb621aa9bd!'
+             '2sShorinji%20Kempo%20Karlstad%20Shibu!5e0!3m2!1ssv!2sse!'
+             '4v1568641561509!5m2!1ssv!2sse"'
+             'width="800" height="450" frameborder="0" style="border:0;"'
+             'allowfullscreen=""></iframe>'
              "Från och med höstterminen 2020 är det meningen att vi "
              "ska börja träna i Kvarnbergsskolans nya gymnastiksal, "
              "som just nu är under byggnation. "],
-            
+
             # Är det svårt
             ["Är Shorinji Kempo svårt",
              "Är det en svår sport",
@@ -142,7 +150,7 @@ class DemoBot():
              "Är det svårt att lära sig",
              "Det finns mycket att lära sig, men det är också det "
              "som gör det intressant."],
-            
+
             # Träningstider
             ["Vilka träningstider har ni",
              "När tränar ni",
@@ -161,9 +169,10 @@ class DemoBot():
              "Träningstiderna hittar du på "
              "<a href='http://shorinjikempo.net/traning/traningstider'>"
              "vår hemsida</a>."],
-            
+
             # Åldersgränser
             ["Hur gammal måste man vara",
+             "Hur gammal måste man vara för att få börja",
              "Vad har ni för åldersgränser",
              "Har ni någon åldersgräns",
              "För liten",
@@ -184,7 +193,7 @@ class DemoBot():
              "aktiva medlemmar som är 60+. De som är under "
              "13 år tränar i juniorgruppen, resten tränar i "
              "vuxengruppen. "],
-            
+
             # Måste man kunna japanska
             ["Måste man kunna japanska",
              "Behöver man lära sig japanska",
@@ -192,7 +201,7 @@ class DemoBot():
              "Man behöver inte kunna japanska för att träna "
              "Shorinji Kempo, men vissa ord och fraser kommer man "
              "att lära sig av sig själv."],
-            
+
             # Vem är tränare
             ["Vem är tränaren",
              "Vem är instruktör",
@@ -210,7 +219,7 @@ class DemoBot():
              "Vår huvudinstruktör heter Anders Pettersson, "
              "och han har graden rokudan - svart bälte av "
              "sjätte graden."],
-            
+
             # När grundades klubben
             ["När grundades klubben",
              "Vilket år grundades klubben",
@@ -222,25 +231,25 @@ class DemoBot():
              "Hur länge har klubben funnits",
              "Klubben grundades av bland andra Anders Pettersson, "
              "1981."],
-            
+
             # Hur många medlemmar har vi
             ["Hur många medlemmar har ni",
              "Hur stort är medlemsantalet",
              "Hur många aktiva medlemmar har ni i klubben",
              "Hur många är ni",
              "I nuläget har vi c:a 40 aktiva medlemmar i vår klubb."],
-            
+
             # Hur många aktiva utövare i Sverige
             ["Hur många aktiva finns det i Sverige",
              "Hur många medlemmar finns det totalt i Sverige",
              "Totalt finns det c:a 300 aktiva utövare i Sverige."],
-            
+
             # Hur många klubbar finns det i Sverige
             ["Hur många klubbar finns det i Sverige",
              "Hur många föreningar finns det i Sverige",
              "Det finns ett tiotal Shorinji Kempo-föreningar i "
              "Sverige."],
-            
+
             # Kontaktuppgifter
             ["Hur kontaktar man er",
              "Hur kan man kontakta er",
@@ -258,7 +267,7 @@ class DemoBot():
              "Våra kontaktuppgifter hittar du på "
              "<a href='http://shorinjikempo.net/kontakt'>"
              "kontaktsidan på vår hemsida</a>."],
-            
+
             # Hemsida
             ["Var finns er hemsida",
              "Har ni någon hemsida",
@@ -266,7 +275,7 @@ class DemoBot():
              "Vad är er hemsideadress",
              "Vår hemsida finns på <a href='http://www.shorinjikempo.net'>"
              "www.shorinjikempo.net</a>."],
-            
+
             # Facebook
             ["Finns ni på Facebook",
              "Har ni någon Facebook-sida",
@@ -277,7 +286,7 @@ class DemoBot():
              "Vår Facebook-sida finns på "
              "<a href='http://www.facebook.com/ShorinjiKempoKarlstad'>"
              "www.facebook.com/ShorinjiKempoKarlstad</a>."],
-            
+
             # Svart bälte
             ["Kan man få svart bälte",
              "Hur lång tid tar det att få svart bälte",
@@ -285,7 +294,7 @@ class DemoBot():
              "Hur mycket träning krävs det för att få svart bälte",
              "Om man ligger i, kan man ta svart bälte efter ungefär "
              "fem års träning."],
-            
+
             # Behöver man ta med sig något till träningen
             ["Behöver man ta med sig något till träningen",
              "Vad behöver man ta med sig till träning",
@@ -298,13 +307,13 @@ class DemoBot():
              "klubben, men innan dess kan man ha vanliga träningskläder "
              "på sig när man tränar. Det kan dock vara en bra ide att "
              "ha en vattenflaska med sig."],
-        
+
             # Behöver man skor
             ["Behöver man ha skor",
              "Har man skor på sig",
              "Får man ha skor när man tränar",
              "Shorinji Kempo tränar man barfota,"],
-            
+
             # Dogi
             ["Var kan man köpa kläder",
              "Måste man ha gi",
@@ -314,7 +323,7 @@ class DemoBot():
              "När man börjar, så kan man använda vanliga "
              "träningskläder. Sen kan man köpa en traditionell "
              "träningsdräkt - en dogi - genom klubben."],
-            
+
             # Skador
             ["Är det lätt att skada sig",
              "Hur stor är skaderisken",
@@ -326,7 +335,7 @@ class DemoBot():
              "Kan man bli skadad",
              "Skaderisken är relativt låg i Shorinji Kempo, jämfört "
              "med t.ex. fotboll eller innebandy."],
-            
+
             # Hur många bälten finns det
             ["Hur många bälten finns det",
              "Vad finns det för bälten",
@@ -336,7 +345,7 @@ class DemoBot():
              "om man är över 10 år). Sedan kommer det grönt bälte i "
              "tre grader (alltså tre separata graderingar), sedan "
              "brunt i tre grader, och till sist svart i nio grader."],
-            
+
             # Nybörjargrupp
             ["Har ni nybörjargrupper",
              "När tränar nybörjare",
@@ -350,7 +359,7 @@ class DemoBot():
              "hjälper till att instruera nybörjarna, så alla tränar "
              "på samma tider. Därför kan man börja träna även mitt i "
              "en termin."],
-            
+
             # Familjerabatt
             ["Har ni familjerabatt",
              "Finns det familje rabatt",
@@ -363,7 +372,7 @@ class DemoBot():
              "erbjuder vi familjerabatter - se "
              "<a href='http://www.shorinjikempo.net/traning/"
              "traningsavgifter>vår hemsida</a> för alla detaljer."],
-            
+
             # Bussar
             ["Går det bussar till träningslokalen",
              "Vilken buss ska man ta till dojon",
@@ -371,7 +380,7 @@ class DemoBot():
              "Man kan förslagsvis åka buss nummer 3 från torget, "
              "men alla bussar som går till Sjukhuset stannar nära "
              "träningslokalen."],
-            
+
             # Parkering
             ["Finns det parkeringsplatser vid träningslokalen",
              "Går det att parkera",
@@ -385,7 +394,7 @@ class DemoBot():
              "Mariebergsskolan, precis utanför vår ytterdörr. "
              "Ibland kan det hända att det blir fullt där, då kan "
              "man lämpligtvis parkera på Värmlandsarkivs parkering."],
-            
+
             # Tävlingar
             ["Brukar ni tävla",
              "Finns det tävlingar i Shorinji Kempo",
@@ -407,7 +416,7 @@ class DemoBot():
              'frameborder="0" allow="accelerometer; autoplay; '
              'encrypted-media; gyroscope; picture-in-picture" '
              'allowfullscreen></iframe>'],
-            
+
             # Hur ofta tävlar vi
             ["Hur ofta är det tävling",
              "Hur ofta brukar ni tävla",
@@ -417,7 +426,7 @@ class DemoBot():
              "Är det många tävlingar",
              "Vi brukar delta i SM varje år, och vi har även lösa "
              "planer på att arrangera egna klubbtävlingar."],
-            
+
             # Sparring
             ["Har ni sparring",
              "Brukar ni sparras",
@@ -429,7 +438,7 @@ class DemoBot():
              "Sparring inom Shorinji Kempo - och inom japanska "
              "kampsporter i allmänhet - kallas randori, och det är "
              "något vi tränar regelbundet i vuxengruppen."],
-            
+
             # Barngrupp
             ["Har ni barngrupper",
              "Har ni någon barngrupp",
@@ -450,7 +459,8 @@ class DemoBot():
             train_sentences = data[:-1]
             response_data = data[-1]
 
-            intent = nlp.Intent(name=intent_name, train_sentences=train_sentences,
+            intent = nlp.Intent(name=intent_name,
+                                train_sentences=train_sentences,
                                 response_data=response_data)
             self.all_intents.append(intent)
 
@@ -483,13 +493,14 @@ class DemoBot():
 def home():
     return render_template("home.html")
 
+
 @app.route("/get")
 def get_bot_response():
     user_text = request.args.get("msg")
     return str(bot.get_response(user_text))
 
+
 if __name__ == "__main__":
     bot = DemoBot()
 
     app.run(debug=False)
-    
